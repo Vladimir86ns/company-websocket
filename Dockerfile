1b7075b4f35bef0c07199b94e64ea76fd5266668
vladimir86ns <vladimir86ns@gmail.com>
2019-07-29 18:26:40 +0200

    fix docker file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM node:10.16.0-jessie
-WORKDIR /var/www/meridianbet-sails-deepstreams
+FROM node:10.16.0
+WORKDIR /var/www/websocket-deepstream
 ADD . .
 RUN npm i
 CMD [ "node", "server.js" ]
+EXPOSE 6020
--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const DeepstreamServer = require('deepstream.io');
-const C = DeepstreamServer.constants;
 
-/*
-The server can take
-1) a configuration file path
-2) null to explicitly use defaults to be overriden by server.set()
-3) left empty to load the base configuration from the config file located within the conf directory.
-4) pass some options, missing options will be merged with the base configuration
-*/
 const options = {
     connectionEndpoints: {
         websocket: {
             options: {
-                port: 3020
+                port: 6020
             }
         },
-        http: {
-            options: {
-                port: 3040
-            }
-        }
     }
 }
 
 const server = new DeepstreamServer(options);
 
-// start the server
 server.start()
 
